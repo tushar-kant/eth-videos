564d6886d1ac919a91b055867a09ee9fc72adc9a
Tushar kanta Nayak <85495427+tushar-kant@users.noreply.github.com>
2022-02-24 08:16:57 +0530

    Update DVideo.sol

--- src/contracts/DVideo.sol
+++ src/contracts/DVideo.sol
@@ -23,19 +23,19 @@ contract DVideo {
   }
 
   function uploadVideo(string memory _videoHash, string memory _title) public {
-    // Make sure the video hash exists
+  
     require(bytes(_videoHash).length > 0);
-    // Make sure video title exists
+
     require(bytes(_title).length > 0);
-    // Make sure uploader address exists
+  
     require(msg.sender!=address(0));
 
-    // Increment video id
+  
     videoCount ++;
 
-    // Add video to the contract
+
     videos[videoCount] = Video(videoCount, _videoHash, _title, msg.sender);
-    // Trigger an event
+ 
     emit VideoUploaded(videoCount, _videoHash, _title, msg.sender);
   }
 }
